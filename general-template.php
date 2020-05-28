e58a008592094f5312e72feafb8350182e844ce8
Zafer ATLI <54990817+zaferatli@users.noreply.github.com>
2020-05-28 14:09:28 +0300

    Add files via upload

--- general-template.php
+++ general-template.php
@@ -4186,7 +4186,7 @@ function paginate_links( $args = '' ) {
 				$link .= $args['add_fragment'];
 
 				$page_links[] = sprintf(
-					'<a class="page-numbers" href="%s"',
+					'<a class="page-numbers" href="%s">%s</a>',
 					/** This filter is documented in wp-includes/general-template.php */
 					esc_url( apply_filters( 'paginate_links', $link ) ),
 					$args['before_page_number'] . number_format_i18n( $n ) . $args['after_page_number']
